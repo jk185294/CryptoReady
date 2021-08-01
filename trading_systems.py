c84c8b1acaebbe90fce07647d9e4817210ccb21c
Jeff Kim <jk185294@ncr.com>
2021-08-01 00:31:20 -0400

    added some comments to help readers
    added some comments to help readers

--- trading_systems.py
+++ trading_systems.py
@@ -1,11 +1,11 @@
 import cbpro
 import constants
- 
+
+#Public Client ******************************************************************************************************
+
 _client = cbpro.PublicClient()
- 
-tick = _client.get_product_ticker(product_id='BTC-USD')
- 
-print(tick['bid'])
+
+# Authenticated Client
 
 auth_client = cbpro.AuthenticatedClient(constants.SANDBOX_KEY, constants.SANDBOX_B64SECRET, constants.SANDBOX_PASSPHRASE,
                                   api_url="https://api-public.sandbox.pro.coinbase.com")
@@ -13,37 +13,73 @@ auth_client = cbpro.AuthenticatedClient(constants.SANDBOX_KEY, constants.SANDBOX
 
 asset = auth_client.get_accounts()
 
+#Public Client Methods ******************************************************************************************************
+
 # print(asset)
 
-order_book = auth_client.get_product_order_book('BTC-USD')
+order_book = _client.get_product_order_book('BTC-USD')
 
 # print(order_book)
 
-product_ticker = auth_client.get_product_ticker(product_id='BTC-USD')
+product_ticker = _client.get_product_ticker(product_id='BTC-USD')
 
 # print(product_ticker)
 
-product_trades = auth_client.get_product_trades(product_id='BTC-USD')
-
-print(product_trades)
+product_trades = _client.get_product_trades(product_id='BTC-USD')
 
 
-# auth_client.buy(price='40000', #USD
+# generator??? ******************************************************************************************************
+# print(product_trades)
+
+product_historic_rates = _client.get_product_historic_rates(product_id='BTC-USD')
+
+# print(product_historic_rates)
+
+product_24hr_stats = _client.get_product_24hr_stats('ETH-USD')
+
+
+# why NotFound when ETH-USD? ******************************************************************************************************
+# print(product_24hr_stats)
+
+currencies = _client.get_currencies()
+
+# print(currencies)
+
+time = _client.get_time()
+
+# print(time)
+
+
+tick = _client.get_product_ticker(product_id='BTC-USD')
+ 
+# print(tick['bid'])
+
+
+# def get_fills(self, product_id=None, order_id=None, **kwargs):
+
+# fills_gen = auth_client.get_fills()
+# Get all fills (will possibly make multiple HTTP requests)
+# all_fills = list(fills_gen)
+
+
+
+
+# _client.buy(price='40000', #USD
 #                size='5', #BTC
 #                order_type='limit',
 #                product_id='BTC-USD')
 
-    # auth_client = cbpro.PublicClient()
+    # _client = cbpro.PublicClient()
  
-    # tick = auth_client.get_product_ticker(product_id='BTC-USD')
+    # tick = _client.get_product_ticker(product_id='BTC-USD')
  
     # print(tick['bid'])
-    # # auth_client = cbpro.AuthenticatedClient(constants.SANDBOX_KEY, 
+    # # _client = cbpro.AuthenticatedClient(constants.SANDBOX_KEY, 
     # #         constants.SANDBOX_PASSPHRASE, 
     # #         constants.SANDBOX_B64SECRET,
     # #     api_url="https://docs.pro.coinbase.com/#sandbox")
 
-    # # tick = auth_client.get_product_ticker(product_id='BTC-USD')
+    # # tick = _client.get_product_ticker(product_id='BTC-USD')
 
     # # print(tick['bid'])
 

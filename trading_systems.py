57313e8f888fd1ae2e7383a34e05af512e527afa
Jeff Kim <jk185294@ncr.com>
2021-07-31 23:51:06 -0400

    Testing several functions from API
    Testing several functions from API

--- trading_systems.py
+++ trading_systems.py
@@ -10,12 +10,28 @@ print(tick['bid'])
 auth_client = cbpro.AuthenticatedClient(constants.SANDBOX_KEY, constants.SANDBOX_B64SECRET, constants.SANDBOX_PASSPHRASE,
                                   api_url="https://api-public.sandbox.pro.coinbase.com")
 
-auth_client.buy(price='40000', #USD
-               size='5', #BTC
-               order_type='limit',
-               product_id='BTC-USD')
-    # import cbpro
-    # # import constants
+
+asset = auth_client.get_accounts()
+
+# print(asset)
+
+order_book = auth_client.get_product_order_book('BTC-USD')
+
+# print(order_book)
+
+product_ticker = auth_client.get_product_ticker(product_id='BTC-USD')
+
+# print(product_ticker)
+
+product_trades = auth_client.get_product_trades(product_id='BTC-USD')
+
+print(product_trades)
+
+
+# auth_client.buy(price='40000', #USD
+#                size='5', #BTC
+#                order_type='limit',
+#                product_id='BTC-USD')
 
     # auth_client = cbpro.PublicClient()
  
